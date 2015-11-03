232c031c451f0201caa394de1bf4a2bde3dcc50d
prodrig <pablo.rodriguez.perez@cern.ch>
2015-11-03 11:25:16 +0000

    added line readout for every 10K line

--- scramble_sim_cpp/root_transition_analysis.cpp
+++ scramble_sim_cpp/root_transition_analysis.cpp
@@ -2,21 +2,25 @@
 
 void graph_init(int data_points)
 {
+  if (argc>=3)
   histogram[0] = new TH1F(
 			  "desync9X_scrambler_hist",
 			  "Transitions per Frame in Desync9X-Scrambled Data",
 			  60,30,90
 			  );
+  if (argc>=4)
   histogram[1] = new TH1F(
 			  "Karol_scrambler_hist",
 			  "Transitions per Frame in New-Scrambled Data",
 			  60,30,90
 			  );
+  if (agrc>=5)
   histogram[2] = new TH1F(
 			  "additive_scrambler_hist",
 			  "Transitions per Frame in Additive-Scrambled Data",
 			  60,30,90
 			  );
+  if (argc>=6)
   histogram[3] = new TH1F(
 			  "Velopix_scrambler_hist",
 			  "Transitions per Frame in Additive-Scrambled Data",
@@ -27,11 +31,16 @@ void graph_init(int data_points)
 void draw_graphs()
 {
   for (int j(0); j<input_file.size(); j++)
+    {
+      int count(0);
     while (!input_file[j]-eof())
       {
 	string input;
 	input_file[j] >> temp;
 	histogram[j] -> Fill(transition_count(str_to_frame(input)));
+
+	count ++;
+	if (!count % 10000) cout << histogram[j].getName() << count << endl;
       }
 }
 
