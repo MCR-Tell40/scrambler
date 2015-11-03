f1f995f94cbc870d387b6d502cb3fa184d32cc8f
nmead <nmead@rubbia.hep.manchester.ac.uk>
2015-11-03 17:22:21 +0000

    chain analysis now working - nick

--- scramble_sim_cpp/root_longest_chain.cpp
+++ scramble_sim_cpp/root_longest_chain.cpp
@@ -1,102 +1,88 @@
 #include "root_longest_chain.h"
 
-int count_data_points()
+void graph_init()
 {
-  int count(0);
-  while(!un_scrambled_datafile->eof())
+  if (input_file.size()>=1)
+    histogram.push_back( new TH1F(
+				  "desync9X_scrambler_hist",
+				  "Chain Lengths in Desync9X-Scrambled Data",
+				  30,0,30
+				  ));
+  if (input_file.size()>=2)
+    histogram.push_back( new TH1F(
+				  "Karol_scrambler_hist",
+				  "Chain Lengths in New-Scrambled Data",
+				  30,0,30
+				  ));
+  if (input_file.size()>=3)
+    histogram.push_back( new TH1F(
+				  "additive_scrambler_hist",
+				  "Chain Lengths in Additive-Scrambled Data",
+				  30,0,30
+				  ));
+  if (input_file.size()>=4)
+    histogram.push_back( new TH1F(
+				  "Velopix_scrambler_hist",
+				  "Chain Lengths in Additive-Scrambled Data",
+				  30,0,30
+				  ));
+}
+
+void draw_graphs()
+{
+  for (int i(0); i < input_file.size(); i++)
     {
-      string temp;
-      *un_scrambled_datafile >> temp;
-      count++;
-    }
-  return count;
-}
-
-void graph_init(int data_point)
-{
-  un_scrambled_hist = new TH1F("Un_Scramble_Longest_Chain","Un_Scramble_Longest_Chain",35,0,35);
-  new_scrambled_hist = new TH1F("New_Scramble_Longest_Chain","New_Scramble_Longest_Chain",35,0,35);
-  old_scrambled_hist = new TH1F("Old_Scramble_Longest_Chain","Old_Scramble_Longest_Chain",35,0,35);
-}
-
-void draw_graphs(int data_points)
-{
-  for (int i(0); i < data_points-1; i++)
-    {
-
-      string temp;
-      *un_scrambled_datafile >> temp;
-      longest_chain_count(str_to_packet(temp),un_scrambled_hist);
-      *new_scrambled_datafile >> temp;
-      longest_chain_count(str_to_packet(temp),new_scrambled_hist);
-      *old_scrambled_datafile >> temp;
-      longest_chain_count(str_to_packet(temp),old_scrambled_hist);
-
-      //    string temp;
-      //    *un_scrambled_datafile >> temp;
-      //    if (longest_chain_count(str_to_packet(temp)!=0))
-      //	un_scrambled_hist -> Fill(longest_chain_count(str_to_packet(temp)));
-      //    *new_scrambled_datafile >> temp;
-      //    if (longest_chain_count(str_to_packet(temp)!=0))
-      //	new_scrambled_hist -> Fill(longest_chain_count(str_to_packet(temp)));
-      //    *old_scrambled_datafile >> temp;
-      //    if (longest_chain_count(str_to_packet(temp)!=0))
-      //	old_scrambled_hist -> Fill(longest_chain_count(str_to_packet(temp)));
+#ifdef __TRANSITION_DEBUG__
+      cout << "draw_graphs()" << endl;
+#endif
+      int count(0);
+      while (!input_file[i]->eof())
+	{
+	  string input;
+	  *input_file[i] >> input;
+	  chain_count(str_to_frame(input),histogram[i]);
+      
+	  count ++;
+	  if (count%10000 == 0) cout << histogram[i]->GetName() << count << endl;
+	}
     }
 }
 
-packet str_to_packet(string signal)
+frame str_to_frame(string signal)
 {
-  packet _packet = 0;
+  frame _frame = 0;
     
   for (int index(1); index <= 128; index++)
     if (signal[128-index] == '1')
-      _packet[index-1] = 1;
+      _frame[index-1] = 1;
     
-  return _packet;
+  return _frame;
 }
 
-
-
-
-int longest_chain_count(packet DATA, TH1F* histogram) //IN PROGRESS
+int chain_count(frame DATA, TH1F* histogram) //IN PROGRESS
 { 
-  int a(1);
-  // int b(1);
-  //int longest_chain(1);
+  int chain_length(1);
   for (int i(1); i < 120 ; i++)
     {
-      if (DATA[i] == DATA[i-1] && DATA[i] == 0)
+      if (DATA[i] == DATA[i-1])
+	//if (DATA[i] == 0)  //chains of 0's
+	//if (DATA[i] == 1)  //chains of 1's
 	{
-	  a = a+1;	 
+	  chain_length = chain_length+1;	 
 	}
-      // else if (DATA[i] == DATA[i-1] && DATA[i] == 1)
-      //{
-      // b=b+1;
-      //}
       else
 	{
-	 histogram -> Fill(a);	  
-	  a = 1;
-	  // b = 1;
-	  // cout << b << endl;
+	 histogram -> Fill(chain_length);	  
+	  chain_length = 1;
 	}         
-      //b = a;
-      //if (b > longest_chain)
-      //longest_chain = b;
     }
   return 0;
 }
 
-
-
-
-
 void save_graphs()
 {
   output_file -> cd();
-  un_scrambled_hist -> Write("Un_Scramble_Longest_Chain");
-  new_scrambled_hist -> Write("New_Scramble_Longest_Chain");
-  old_scrambled_hist -> Write("Old_Scramble_Longest_Chain");
+  for (auto& hist: histogram)
+    hist->Write();
   output_file->Close();
 }
