7c6028aac0402425fdf6304974cf65cff668adbe
IamNickMead <n1ck.mead@yahoo.co.uk>
2015-12-16 08:20:00 +0000

    graphs complete

--- scramble_sim_cpp/draw_balance_graphs_X.C
+++ scramble_sim_cpp/draw_balance_graphs_X.C
@@ -1,47 +1,93 @@
 void draw_balance_graphs_X()
 {
-  TFile* f = new TFile("Balance_all.root");
+  TFile* f = new TFile("balance_analysis_X.root");
 
-  // TGraph* desync9X_graph = (TGraph*)f->Get("/home/velo/Desktop/desync/desyncX.txt");
-  TGraph* true_Karol_graph = (TGraph*)f->Get("true_allKarol.txt");
-  TGraph* true_additive_graph = (TGraph*)f->Get("true_allAdd.txt");
-  TGraph* true_velopix_graph = (TGraph*)f->Get("true_allVelo.txt");
-  TGraph* abs_Karol_graph = (TGraph*)f->Get("abs_allKarol.txt");
-  TGraph* abs_additive_graph = (TGraph*)f->Get("abs_allAdd.txt");
-  TGraph* abs_velopix_graph = (TGraph*)f->Get("abs_allVelo.txt");
+  TGraph* true_desyncX_graph = (TGraph*)f->Get("true_desyncX.txt");
+  TGraph* true_Karol_graph = (TGraph*)f->Get("true_Karol_scramble_output_x.txt");
+  TGraph* true_additive_graph = (TGraph*)f->Get("true_additive_scramble_output_x.txt");
+  TGraph* true_velopix_graph = (TGraph*)f->Get("true_VeloPix_scramble_output_x.txt");
+  TGraph* true_random_data = (TGraph*)f->Get("true_random_data_output_x.txt");
+  TGraph* abs_desyncX_graph = (TGraph*)f->Get("abs_desyncX.txt");
+  TGraph* abs_Karol_graph = (TGraph*)f->Get("abs_Karol_scramble_output_x.txt");
+  TGraph* abs_additive_graph = (TGraph*)f->Get("abs_additive_scramble_output_x.txt");
+  TGraph* abs_velopix_graph = (TGraph*)f->Get("abs_VeloPix_scramble_output_x.txt");
+  TGraph* abs_random_data = (TGraph*)f->Get("abs_random_data_output_x.txt");
+
+  vector<double> x, y, ex, ey1, ey2;
+
+  int n = true_velopix_graph->GetN();
+  int div = 1;
+  n = n / div;
+
+  for(int i(0); i < n; i += div)
+  {
+    x.push_back(i);
+    y.push_back(0);
+    ex.push_back(0);
+    ey1.push_back(sqrt(i * 120));
+    ey2.push_back(2* sqrt(i * 120));
+  }
+
+  TGraphErrors * Sigma1 = new TGraphErrors(n,&x[0],&y[0],&ex[0],&ey1[0]);
+  TGraphErrors * Sigma2 = new TGraphErrors(n,&x[0],&y[0],&ex[0],&ey2[0]);
+  TGraph * Xaxis = new TGraph(n,&x[0],&y[0]);
 
   TCanvas* true_c1 = new TCanvas("Evolution in the Balance of 1s and 0s",
 			    "Evolution in the Balance of 1s and 0s",
-			    2700,1800);
-  true_c1->Pad()->SetGridy();
+			    900,600);
+  true_c1->SetTicks();
 
-  true_additive_graph->Draw("AL");
+  TMultiGraph * true_mg = new TMultiGraph();
+  TMultiGraph * abs_mg = new TMultiGraph();
+
+  Sigma2->SetFillColor(5);
+  Sigma2->SetLineColor(5);
+  Sigma1->SetFillColor(3);
+  Sigma1->SetLineColor(3);
+
+  true_mg->Add(Sigma2);
+  true_mg->Add(Sigma1);
+  true_mg->Draw("a3");
+
+  Xaxis->Draw("same");
+
+  true_mg->GetXaxis()->SetTitle("Frame #");
+  true_mg->GetYaxis()->SetTitle("(#1 - #0)");
+  true_mg->GetYaxis()->SetRangeUser(-6e4,1e5);
+  true_mg->GetXaxis()->SetRangeUser(0,n*div);
+  true_mg->SetTitle("Evolution in the Balance of 1s and 0s");
+
+  true_additive_graph->Draw("same");
   true_additive_graph->SetLineColor(4);
   true_additive_graph->SetLineWidth(2);
-  true_additive_graph->GetXaxis()->SetTitle("Frame #");
-  true_additive_graph->GetYaxis()->SetTitle("(#1 - #0)");
-  true_additive_graph->GetYaxis()->SetRangeUser(-6e4,1e5);
-  //  true_additive_graph->GetXaxis()->SetRangeUser(0,335e3);
-  true_additive_graph->SetTitle("Evolution in the Balance of 1s and 0s");
 
   true_Karol_graph->Draw("same");
   true_Karol_graph->SetLineColor(2);
   true_Karol_graph->SetLineWidth(2);
   true_Karol_graph->SetMarkerStyle(0);
 
-  //desync9X_graph->Draw("same");
-  //desync9X_graph->SetLineColor(6);
-  //desync9X_graph->SetLineWidth(2);
+  true_desyncX_graph->Draw("same");
+  true_desyncX_graph->SetLineColor(6);
+  true_desyncX_graph->SetLineWidth(2);
 
   true_velopix_graph->Draw("same");
   true_velopix_graph->SetLineColor(7);
   true_velopix_graph->SetLineWidth(2);
 
-  TLegend* true_leg = new TLegend(0.7,0.82,0.9,0.9);
-  //  true_leg->True_AddEntry(desync9X_graph,"Dysync 9X","l");
-  true_leg->AddEntry(true_velopix_graph,"VeloPix Scrambler","l");
-  true_leg->AddEntry(true_Karol_graph,"Intermediate Scrambler","l");
+  true_random_data->Draw("same");
+  true_random_data->SetLineColor(1);
+  true_random_data->SetLineWidth(2);
+
+  TLegend* true_leg = new TLegend(0.13,0.55,0.25,0.87);
+  true_leg->SetLineColor(0);
+  true_leg->SetTextSize(0.0275);
+  true_leg->AddEntry(true_desyncX_graph,"Unscrambled Data","l");
   true_leg->AddEntry(true_additive_graph,"Additive Scrambler","l");
+  true_leg->AddEntry(true_Karol_graph,"Intermediate Scrambler","l");
+  true_leg->AddEntry(true_velopix_graph,"VeloPix Scrambler","l");
+  true_leg->AddEntry(true_random_data,"Random Data","l");
+  true_leg->AddEntry(Sigma1,"#pm 1 #sigma","f");
+  true_leg->AddEntry(Sigma2,"#pm 2 #sigma","f");
   true_leg->Draw();
 
  
@@ -52,37 +98,51 @@ void draw_balance_graphs_X()
 
   TCanvas* abs_c1 = new TCanvas("Evolution in the Absolute Value of Balance of 1s and 0",
 			    "Evolution in the Absolute Value of Balance of 1s and 0s",
-			    2700,1800);
-  abs_c1->Pad()->SetGridy();
+			    0,800,900,600);
+  abs_c1->SetTicks();
 
-  abs_additive_graph->Draw("AL");
+  abs_mg->Add(Sigma2);
+  abs_mg->Add(Sigma1);
+  abs_mg->Draw("a3");
+
+  abs_mg->GetXaxis()->SetTitle("Frame #");
+  abs_mg->GetYaxis()->SetTitle("abs(#1 - #0)");
+  abs_mg->GetYaxis()->SetRangeUser(0,1e5);
+  abs_mg->GetXaxis()->SetRangeUser(0,n*div);
+  abs_mg->SetTitle("Evolution in the Balance of 1s and 0s");
+
+  abs_additive_graph->Draw("same");
   abs_additive_graph->SetLineColor(4);
   abs_additive_graph->SetLineWidth(2);
-  abs_additive_graph->GetXaxis()->SetTitle("Frame #");
-  abs_additive_graph->GetYaxis()->SetTitle("abs(#1 - #0)");
-  abs_additive_graph->GetYaxis()->SetRangeUser(0,100000);
-  //  abs_additive_graph->GetXaxis()->SetRangeUser(0,335e3);
-  abs_additive_graph->SetTitle("Evolution in the Absolute Value of Balance of 1s and 0s");
 
   abs_Karol_graph->Draw("same");
   abs_Karol_graph->SetLineColor(2);
   abs_Karol_graph->SetLineWidth(2);
   abs_Karol_graph->SetMarkerStyle(0);
 
-  //desync9X_graph->Draw("same");
-  //desync9X_graph->SetLineColor(6);
-  //desync9X_graph->SetLineWidth(2);
+  abs_desyncX_graph->Draw("same");
+  abs_desyncX_graph->SetLineColor(6);
+  abs_desyncX_graph->SetLineWidth(2);
 
   abs_velopix_graph->Draw("same");
   abs_velopix_graph->SetLineColor(7);
   abs_velopix_graph->SetLineWidth(2);
 
-  TLegend* abs_leg = new TLegend(0.7,0.82,0.9,0.9);
-  //  abs_leg->Abs_AddEntry(desync9X_graph,"Dysync 9X","l");
-  abs_leg->AddEntry(abs_velopix_graph,"VeloPix Scrambler","l");
-  abs_leg->AddEntry(abs_Karol_graph,"Intermediate Scrambler","l");
-  abs_leg->AddEntry(abs_additive_graph,"Additive Scrambler","l");
-  abs_leg->Draw();
+  abs_random_data->Draw("same");
+  abs_random_data->SetLineColor(1);
+  abs_random_data->SetLineWidth(2);
+
+  // TLegend* abs_leg = new TLegend(0.13,0.7,0.5,0.87);
+  // abs_leg->SetLineColor(0);
+  // abs_leg->AddEntry(abs_desyncX_graph,"Unscrambled Data","l");
+  // abs_leg->AddEntry(abs_additive_graph,"Additive Scrambler","l");
+  // abs_leg->AddEntry(abs_Karol_graph,"Intermediate Scrambler","l");
+  // abs_leg->AddEntry(abs_velopix_graph,"VeloPix Scrambler","l");
+  // abs_leg->AddEntry(abs_random_data,"Random Data","l");
+  // abs_leg->AddEntry(Sigma1,"#pm 1 #sigma","f");
+  // abs_leg->AddEntry(Sigma2,"#pm 2 #sigma","f");
+  // abs_leg->Draw();
+  true_leg->Draw();
 
  
   abs_c1->Update();
--- scramble_sim_cpp/draw_chain_graph_full.C
+++ scramble_sim_cpp/draw_chain_graph_full.C
@@ -1,6 +1,6 @@
 Double_t linFit(Double_t * x, Double_t * par)
 {
-  return par[0] + TMath::Power(par[1],x[0]);
+  return par[0] + TMath::Power(10,x[0]*par[1]);
 };
 
 void draw_chain_graph_full()
@@ -18,80 +18,112 @@ void draw_chain_graph_full()
   // gStyle->SetStatH(0.12);  
   
   TCanvas *c1 = new TCanvas("c1","Chain Length",900,600);
+  c1->GetFrame()->SetBorderSize(0);
 
-for (int i(0); i < Karol_scramble_hist->GetNbinsX(); i++)
-  {
-    Karol_scramble_hist->SetBinContent(i,TMath::Log(Karol_scramble_hist->GetBinContent(i)));
-    un_scramble_hist->SetBinContent(i,log(un_scramble_hist->GetBinContent(i)));
-    additive_scramble_hist->SetBinContent(i,log(additive_scramble_hist->GetBinContent(i)));
-    Velopix_scramble_hist->SetBinContent(i,log(Velopix_scramble_hist->GetBinContent(i)));
-    Random_data_hist->SetBinContent(i,log(Random_data_hist->GetBinContent(i)));
-  }
+// for (int i(0); i < Karol_scramble_hist->GetNbinsX(); i++)
+//   {
+//     Karol_scramble_hist->SetBinContent(i,TMath::Log(Karol_scramble_hist->GetBinContent(i)));
+//     un_scramble_hist->SetBinContent(i,log(un_scramble_hist->GetBinContent(i)));
+//     additive_scramble_hist->SetBinContent(i,log(additive_scramble_hist->GetBinContent(i)));
+//     Velopix_scramble_hist->SetBinContent(i,log(Velopix_scramble_hist->GetBinContent(i)));
+//     Random_data_hist->SetBinContent(i,log(Random_data_hist->GetBinContent(i)));
+//   }
 
   // Pre Scramble
   un_scramble_hist->Draw("");
   un_scramble_hist->SetLineColor(6);
   un_scramble_hist->SetLineStyle(1);
-  //un_scramble_hist->Fit("gaus");
-  //un_scramble_hist->GetFunction("gaus")->SetLineColor(1);
+  un_scramble_hist->Fit("expo");
+  un_scramble_hist->GetFunction("expo")->SetLineColor(6);
   un_scramble_hist->SetTitle(0);
   un_scramble_hist->GetXaxis()->SetTitle("Chain Length");
+  un_scramble_hist->GetXaxis()->SetRangeUser(1,30);
   un_scramble_hist->GetYaxis()->SetTitle("Entries");
  
   un_scramble_hist->SetStats(0);
    
-
   // New Scramble
   
 
   Karol_scramble_hist->Draw("sames");
   Karol_scramble_hist->SetLineColor(2);
   Karol_scramble_hist->SetLineStyle(1);
-  TF1* Karol_fit = new TF1("Karol_fit",linFit,1,30,2);
-  Karol_fit->SetParameters(1e7,-0.5);
-  Karol_scramble_hist->Fit("Karol_fit"); 
-  //Karol_scramble_hist->GetFunction("gaus")->SetLineColor(2);
-
+  // TF1* Karol_fit = new TF1("Karol_fit",linFit,1,30,2);
+  // Karol_fit->SetParameters(1e7,-0.5);
+  Karol_scramble_hist->Fit("expo"); 
+  Karol_scramble_hist->GetFunction("expo")->SetLineColor(2);
+  Karol_scramble_hist->GetFunction("expo")->SetLineColor(1);
   //Karol_scramble_hist->GetYaxis()->SetLabelSize(0.02);
   Karol_scramble_hist->SetStats(0);
+
   
   
   // Additive Scramble
   additive_scramble_hist->Draw("sames");
   additive_scramble_hist->SetLineColor(4);
-  additive_scramble_hist->SetLineStyle(7);
-  //additive_scramble_hist->Fit("gaus"); 
-  //additive_scramble_hist->GetFunction("gaus")->SetLineColor(4); 
+  additive_scramble_hist->SetLineStyle(1);
+  additive_scramble_hist->Fit("expo"); 
+  additive_scramble_hist->GetFunction("expo")->SetLineColor(4); 
+  additive_scramble_hist->GetFunction("expo")->SetLineStyle(7);
   additive_scramble_hist->SetStats(0);
 
+  
   // Velopix Scramble
   Velopix_scramble_hist->Draw("sames");
   Velopix_scramble_hist->SetLineColor(7);
-  Velopix_scramble_hist->SetLineStyle(6);
-  // Velopix_scramble_hist->Fit("gaus"); 
-  // Velopix_scramble_hist->GetFunction("gaus")->SetLineColor(4); 
+  Velopix_scramble_hist->SetLineStyle(1);
+  Velopix_scramble_hist->Fit("expo"); 
+  Velopix_scramble_hist->GetFunction("expo")->SetLineColor(7); 
+  Velopix_scramble_hist->GetFunction("expo")->SetLineStyle(6);
   Velopix_scramble_hist->SetStats(0);
 
-
-   // Random Data
+// Random Data
   Random_data_hist->Draw("sames");
   Random_data_hist->SetLineColor(1);
-  Random_data_hist->SetLineStyle(5);
-  Random_data_hist->SetLineWidth(3);
-  //Random_data_hist->Fit("gaus"); 
-  //Random_data_hist->GetFunction("gaus")->SetLineColor(4); 
+  Random_data_hist->SetLineStyle(1);
+  Random_data_hist->Fit("expo"); 
+  Random_data_hist->GetFunction("expo")->SetLineColor(1); 
+  Random_data_hist->GetFunction("expo")->SetLineStyle(5);
   Random_data_hist->SetStats(0);
   
+  double a = un_scramble_hist->GetFunction("expo")->GetParameter(1)*TMath::LogE();
+  double p = 1 - pow(10,a);
+  double e = (p / a) * un_scramble_hist->GetFunction("expo")->GetParError(1) * TMath::LogE();
+  cout << "****Unscrambled: Grad = " << a << "\t p_t = " << p << "\t +/- " << e << endl;
+
+  a = additive_scramble_hist->GetFunction("expo")->GetParameter(1)*TMath::LogE();
+  p = 1 - pow(10,a);
+  e = (p / a) * additive_scramble_hist->GetFunction("expo")->GetParError(1) * TMath::LogE();
+  cout << "****Additive: Grad = " << a << "\t p_t = " << p << "\t +/- " << e << endl;
+
+  a = Karol_scramble_hist->GetFunction("expo")->GetParameter(1)*TMath::LogE();
+  p = 1 - pow(10,a);
+  e = (p / a) * Karol_scramble_hist->GetFunction("expo")->GetParError(1) * TMath::LogE();
+  cout << "****Intermediate: Grad = " << a << "\t p_t = " << p << "\t +/- " << e << endl;
+
+  a = Velopix_scramble_hist->GetFunction("expo")->GetParameter(1)*TMath::LogE();
+  p = 1 - pow(10,a);
+  e = (p / a) * Velopix_scramble_hist->GetFunction("expo")->GetParError(1) * TMath::LogE();
+  cout << "****Velopix: Grad = " << a << "\t p_t = " << p << "\t +/- " << e << endl;
+
+  a = Random_data_hist->GetFunction("expo")->GetParameter(1)*TMath::LogE();
+  p = 1 - pow(10,a);
+  e = (p / a) * Random_data_hist->GetFunction("expo")->GetParError(1) * TMath::LogE();
+  cout << "****Random: Grad = " << a << "\t p_t = " << p << "\t +/- " << e << endl;
+  
   // Legend
-  leg_top = new TLegend(0.65,0.6,0.9,0.9);
-  leg_top->AddEntry(un_scramble_hist,"Pre Scrambler","f");
-  leg_top->AddEntry(Karol_scramble_hist,"Intermediate Scrambler","f");
-  leg_top->AddEntry(additive_scramble_hist,"Additive Scrambler","f");
-  leg_top->AddEntry(Velopix_scramble_hist,"Velopix Scrambler","f");
+  leg_top = new TLegend(0.6,0.6,0.87,0.87);
+  leg_top->SetLineColor(0);
+  leg_top->SetTextSize(0.0275);
+  leg_top->AddEntry(un_scramble_hist,"Unscrambled Data","f");
   leg_top->AddEntry(Random_data_hist,"Random Data","f");
+  leg_top->AddEntry(additive_scramble_hist,"Additive Scrambler","f");
+  leg_top->AddEntry(Karol_scramble_hist,"Intermediate Scrambler","f");
+  leg_top->AddEntry(Velopix_scramble_hist,"VeloPix Scrambler","f");
   leg_top->Draw();
   
-  //c1->SetLogy();
+  c1->SetLogy();
+  c1->SetTicks();
   //c1->SetGridy();
 
   c1->SaveAs("Chain_Length_Full_Hist_Nick.pdf");
